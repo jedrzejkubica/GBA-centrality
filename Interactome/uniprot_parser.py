23a0ad7079d67784656a24edad5049b052dc5f43
jjjk123 <jj.kubica@student.uw.edu.pl>
2025-01-30 19:02:23 +0100

    Interactome/uniprot_parser.py : fixed docs, var names, PEP8

--- Interactome/uniprot_parser.py
+++ Interactome/uniprot_parser.py
@@ -1,39 +1,36 @@
 # This script was cloned from git@github.com:manojmw/grexome-TIMC-Secondary.git
 
+import sys
 import re
 import argparse
 import logging
-import sys
 
 
-###########################################################
+def parse_uniprot_file(uniprot_file):
+    """
+    Parse on STDIN a Uniprot file and extracts the following fields from each record:
+    - Primary Accession and Secondary Accession from the 'AC' line
+    - Gene name and synonyms from the 'GN' line
+    - Taxonomy identifier from the 'OX' line
+    - ENST(s), ENSG(s) & GeneID(s) from the 'DR' line
 
-# Parses on STDIN a Uniprot file and extracts the following fields from each record:
-# - Primary Accession and Secondary Accession(s) from the 'AC' line
-# - Gene Name and synonyms from the 'GN' line
-# - Taxonomy Identifier from the 'OX' line
-# - ENST(s), ENSG(s) & GeneID(s) from the 'DR' line
-#
-# Prints to STDOUT in .tsv format
-# Ouput consists of 7 columns (one record per line):
-# - Uniprot Primary Accession
-# - Taxonomy Identifier
-# - ENST (or a comma seperated list of ENSTs)
-# - ENSG (or a comma seperated list of ENSGs)
-# - Uniprot Secondary Accession (or a comma seperated list of Uniprot Secondary Accessions)
-# - GeneID (or a comma seperated list of GeneIDs)
-# - Gene Name (or a comma seperated list of Gene Names)
-def uniprot_parser(UniProtinFile):
+    Print to STDOUT in TSV format:
+    - Uniprot Primary Accession
+    - Taxonomy identifier
+    - ENST (or a comma seperated list)
+    - ENSG (or a comma seperated list)
+    - Uniprot Secondary Accession (or a comma seperated list)
+    - GeneID (or a comma seperated list)
+    - Gene Name (or a comma seperated list of)
+    """
 
-    logging.info("starting to run")
+    logging.info("Starting to run")
 
-    UniProtinFile = sys.stdin
+    uniprot_file = sys.stdin
 
-    # Header line
-    UniProt_header = ['Primary_AC', 'TaxID', 'ENSTs', 'ENSGs', 'Secondary_ACs', 'GeneIDs', 'GeneNames']
-    print('\t'.join(UniProt_header))
+    header = ['Primary_AC', 'TaxID', 'ENSTs', 'ENSGs', 'Secondary_ACs', 'GeneIDs', 'GeneNames']
+    print('\t'.join(header))
 
-    # Initializing variables/accumulators
     ACs = ''
     TaxID = 0
     ENSTs = []
@@ -41,39 +38,33 @@ def uniprot_parser(UniProtinFile):
     GeneIDs = []
     GeneNames = []
 
-    # Compiling all the regular expressions
-
     # Accession Numbers (AC), strip trailing ';'
     re_AC = re.compile(r'^AC\s+(\S.*);$')
 
     # Gene Name/Synonyms from the GN line
     # the below regex pattern ends with either ; or , because
     # UniProt has a bug (at the time of writing this script)
-    # the lines for some records are incomplete and does not
-    # end with semi-colon as it is supposed to and even unclosed curly braces
-    # Ex:
-    # GN   Name=aadK {ECO:0000303|PubMed:17609790, ECO:0000303|PubMed:8293959,
+    # the lines for some records are incomplete and do not
+    # end with ; as supposed to and even unclosed curly braces
+    # Ex: GN   Name=aadK {ECO:0000303|PubMed:17609790, ECO:0000303|PubMed:8293959,
     # Sometimes, GN lines can also start just with 'Synonyms'
-    # Ex:
-    # GN   Synonyms=AO {ECO:0000303|PubMed:27255930}
+    # Ex: GN   Synonyms=AO {ECO:0000303|PubMed:27255930}
     re_GN = re.compile(r'^GN\s+((Name=\S.*)|(Synonyms=\S.*))[,;]$')
 
-    # Organism (TaxID) from the OX line; some lines violate the uniprot spec
+    # TaxID from the OX line; some lines violate the Uniprot spec
     # Grab only TaxID even if additional info comes after the TaxID
-    # eg NCBI_TaxID=32201 {ECO:0000312|EMBL:ABW86978.1};
+    # Ex: NCBI_TaxID=32201 {ECO:0000312|EMBL:ABW86978.1};
     re_TaxID = re.compile(r'^OX\s+NCBI_TaxID=(\d+)[; ]')
 
-    # Ensembl transcripts and Genes from the DR line
+    # Ensembl transcripts and genes from the DR line
     re_ENS = re.compile(r'^DR\s+Ensembl; (\w+).*; \S+; (\w+).*\.')
 
     # GeneIDs from the DR line
     re_GID = re.compile(r'^DR\s+GeneID;\s+(\d+);')
 
-    # Data lines
-    for line in UniProtinFile:
-        line = line.rstrip('\r\n')  # removing trailing new lines and carriage returns
+    for line in uniprot_file:
+        line = line.rstrip('\r\n')
 
-        # Matching and retrieving the records
         if (re_AC.match(line)):
             if (ACs != ''):
                 # Add trailing separator to the previous entries - distingushes each AC
@@ -85,16 +76,12 @@ def uniprot_parser(UniProtinFile):
             sys.exit()
         elif (re_GN.match(line)):
             GNLine = re_GN.match(line).group(1)
-            # As per the UniProt documentation, the GN
-            # line is supposed to have a defined pattern
-            # but that's not the case in reality
-            # So we grab everything from the GN line
-            # The GN line always ends with a ';' except in some cases where
-            # the records are incomplete and has a bug like below (ends with a ',')
-            # GN   Name=aadK {ECO:0000303|PubMed:17609790, ECO:0000303|PubMed:8293959,
+            # Grab everything from the GN line
+            # The GN line always ends with a ';' except in some cases
+            # Ex: GN   Name=aadK {ECO:0000303|PubMed:17609790, ECO:0000303|PubMed:8293959,
             try:
-                # If the GN line contains any additional info other than
-                # Gene Name, it will also be seperated by a '; '
+                # If the GN line contains any additional info other than Gene Name,
+                # it will also be seperated by a '; '
                 # Ex: GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da; ORFNames=CG7877;
                 GNList = GNLine.split('; ')
                 if GNList:
@@ -102,14 +89,10 @@ def uniprot_parser(UniProtinFile):
                         # Retrieving only the Gene Name
                         if re.match(r'^Name=(\S.*)', geneinfo):
                             GeneNameD = re.match(r'^Name=(\S.*)', geneinfo).group(1)
-                            # Sometimes, Gene Name can contain additional info
-                            # such as pubmed and other accession IDs
-                            # So, we want eliminate these from our
-                            # Gene name
+                            # Remove additional info (eg, Pubmed id) from Gene Name
                             # Ex: GN   Name=dbaA {ECO:0000303|PubMed:23001671};
                             try:
-                                # So we split at the ' {' and keep only the
-                                # Gene Name
+                                # So we split at the ' {' and keep only Gene Name
                                 GeneNamewithAcID = GeneNameD.split(' {')
                                 GeneName = GeneNamewithAcID[0]
                             except Exception:
@@ -117,39 +100,21 @@ def uniprot_parser(UniProtinFile):
                                 # There can be multiple 'GN' lines, especially
                                 # when there are many synonyms
                                 # So we check to avoid adding the same gene name again
-                                # Ex:
-                                # GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da; ORFNames=CG7877;
-                                # GN   Name=Jon99Ciii; Synonyms=SER2, SER5, Ser99Db; ORFNames=CG15519;
+                                # Ex: GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da; ORFNames=CG7877;
+                                # Ex: GN   Name=Jon99Ciii; Synonyms=SER2, SER5, Ser99Db; ORFNames=CG15519;
                             if GeneName not in GeneNames:
                                 GeneNames.append(GeneName)
-                        # retreiving synonyms for Gene Name (if it exists)
+                        # retreiving synonyms for Gene Name
                         if re.match(r'^Synonyms=(\S.*)', geneinfo):
                             GeneSynoinfo = re.match(r'^Synonyms=(\S.*)', geneinfo).group(1)
                             GeneSynonyms = []
                             # There can be multiple synonyms seperated by a ','
-                            # Ex:
-                            # GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da;
+                            # Ex: GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da;
                             try:
-                                # Splitting at ',' can sometimes add additional
-                                # info of synonym to the list as well
-                                # Especially, when more than one additional info
-                                # exists for a given Gene synonym and these are also
-                                # Seperated by a ','. We eliminate this later while
-                                # adding synonyms to the Gene Names list
-                                #
-                                # Gene name Synonym with one additional info
-                                # Ex: GN   Synonyms=ALI2 {ECO:0000250|UniProtKB:Q80ZD8}
-                                #
-                                # Gene name Synonym with more than one additional info
-                                # seperated by a ','
-                                # Ex: GN   Synonyms=OPCL1 {ECO:0000303|PubMed:18267944, ECO:0000303|PubMed:19704801};
                                 GeneSynonymList = GeneSynoinfo.split(', ')
                                 if GeneSynonymList:
-                                    # Like Gene Name, even Gene synonyms can
-                                    # can contain additional info such as pubmed and other accession IDs
-                                    # So, we want eliminate these and keep only the synonym name
-                                    # Ex:
-                                    # GN   Name=Sh3bp5; Synonyms=Sab {ECO:0000303|PubMed:10339589};
+                                    # Remove additional info from Gene Synonyms
+                                    # Ex: GN   Name=Sh3bp5; Synonyms=Sab {ECO:0000303|PubMed:10339589};
                                     for synonym in GeneSynonymList:
                                         try:
                                             Gsynowithaddinfo = synonym.split(' {')
@@ -159,15 +124,13 @@ def uniprot_parser(UniProtinFile):
                                             GeneSynonyms.append(synonym)
                             except Exception:
                                 GeneSynonyms.append(GeneSynoinfo)
-                            # Avoid adding the same synonym again especially
-                            # when they occur on multiple 'GN' lines
                             for synonym in GeneSynonyms:
-                                # Eliminating additional info (i.e not a synonym)
+                                # Remove additional info (i.e not a synonym)
                                 if ':' not in synonym:
                                     if synonym not in GeneNames:
                                         GeneNames.append(synonym)
             except Exception:
-                # if the GN line contains only the Gene name, we do not split
+                # if GN line contains only Gene Name, do not split
                 # Ex: GN   Name=APOM;
                 if re.match(r'^Name=(\S.*)', GNLine):
                     GeneNameD = re.match(r'^Name=(\S+)', GNLine).group(1)
@@ -202,7 +165,7 @@ def uniprot_parser(UniProtinFile):
             if ENSG not in ENSGs:
                 ENSGs.append(ENSG)
         elif (re.match(r'^DR\s+Ensembl;', line)):
-            logging.error("Failed to get all the Ensembl Identifiers \n" + ACs + line)
+            logging.error("Failed to get all the Ensembl identifiers \n" + ACs + line)
             sys.exit()
         elif (re_GID.match(line)):
             GeneIDs.append(re_GID.match(line).group(1))
@@ -210,86 +173,73 @@ def uniprot_parser(UniProtinFile):
             logging.error("Missed the GeneIDs \n" + ACs + line)
             sys.exit()
 
-        # '//' means End of the record
-        # we Process the retreived data
-        elif (line == '//'):
-            # TaxID Human = 9606
-            # ignore entry if bad species;
-            if (TaxID == '9606'):
+        elif (line == '//'):  # '//' end of the record
+            if (TaxID == '9606'):  # Human
                 try:
                     ACs_split = ACs.split('; ')
-                    primary_AC = ACs_split[0]  # Grab only the first AC
-                    secondary_AC_list = ACs_split[1:]  # Grab the remaining ACs
-                    # storing secondary_ACs as a single comma-seperated string
+                    primary_AC = ACs_split[0]
+                    secondary_AC_list = ACs_split[1:]
                     secondary_ACs = ','.join(secondary_AC_list)
                 except Exception:
                     logging.error("Failed to store store UniProt Accession IDs for the protein: \t" + ACs)
                     sys.exit()
+
                 try:
-                    # storing Gene names as a single comma-seperated string
                     GeneNames = ','.join(GeneNames)
                 except Exception:
                     logging.error('Error: Failed to store Gene Name for the protein: \t' + ACs)
                     sys.exit()
+
                 try:
-                    # Processing ENSTs and ENSGs
                     ENSTs = ','.join(ENSTs)
                     ENSGs = ','.join(ENSGs)
                 except Exception:
-                    logging.error("Failed to store Ensembl Identifiers for the protein: \t" + ACs)
+                    logging.error("Failed to store ENST / ENSG for the protein: \t" + ACs)
                     sys.exit()
-                try:
-                    # storing GeneIDs as a single comma-seperated string
-                    GeneIDs = ','.join(GeneIDs)
-                except:
-                    raise Exception("Error: Failed to store Gene Identifiers for the protein: \t' + ACs")
 
-                # Printing to STDOUT
+                try:
+                    GeneIDs = ','.join(GeneIDs)
+                except Exception:
+                    raise Exception("Error: Failed to store Gene identifiers for the protein: \t' + ACs")
+
+                # Print to STDOUT
                 UniProt_outline = [primary_AC, TaxID, ENSTs, ENSGs, secondary_ACs, GeneIDs, GeneNames]
                 print('\t'.join(UniProt_outline))
 
-            # Reset all accumulators and move on to the next record
+            # Reset and move on to the next record
             ACs = ''
             TaxID = 0
             ENSTs = []
             ENSGs = []
             GeneIDs = []
             GeneNames = []
+
             continue
 
-    logging.info("All Done, completed successfully!")
+    logging.info("All done, completed successfully!")
 
     return
 
 
-###########################################################
-
-# Taking and handling command-line arguments
 def main():
     file_parser = argparse.ArgumentParser(description="""
---------------------------------------------------------------------------------------------------
-Program: Parses on STDIN a UniProt file, processes each record and prints to STDOUT in .tsv format
---------------------------------------------------------------------------------------------------
-The output consists of 7 columns:
- -> Uniprot Primary Accession
- -> Taxonomy Identifier
- -> ENST (or a comma seperated list of ENSTs)
- -> ENSG (or a comma seperated list of ENSGs)
- -> Uniprot Secondary Accession (or a comma seperated list of Uniprot Secondary Accessions)
- -> GeneID (or a comma seperated list of GeneIDs)
- -> Gene Name (or a comma seperated list of Gene Names)
---------------------------------------------------------------------------------------------------
+                                          Parse on STDIN a Uniprot file and print to STDOUT in TSV format:
+                                          - Uniprot Primary Accession
+                                          - Taxonomy identifier
+                                          - ENST (or a comma seperated list)
+                                          - ENSG (or a comma seperated list)
+                                          - Uniprot Secondary Accession (or a comma seperated list)
+                                          - GeneID (or a comma seperated list)
+                                          - Gene Name (or a comma seperated list)
+                                          """,
+                                          formatter_class=argparse.RawDescriptionHelpFormatter)
 
-Arguments [defaults] -> Can be abbreviated to shortest unambiguous prefixes
-    """, formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    file_parser.set_defaults(func=uniprot_parser)
+    file_parser.set_defaults(func=parse_uniprot_file)
     args = file_parser.parse_args()
     args.func(args)
 
 
 if __name__ == "__main__":
-    # Logging to Standard Error
     logging.basicConfig(format="%(levelname)s %(asctime)s: %(filename)s - %(message)s",
                         datefmt='%Y-%m-%d %H:%M:%S',
                         stream=sys.stderr,

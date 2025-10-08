1fadd9d17b7f23c2a08f50464771eb844f99da2f
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2025-10-08 12:35:56 +0200

    GBA_centrality.py: pass pathToCode so gbaCentrality.so can be found

--- GBA_centrality.py
+++ GBA_centrality.py
@@ -51,7 +51,7 @@ class GeneScores(ctypes.Structure):
                 ('scores', ctypes.POINTER(SCORETYPE))]
 
 
-def calculate_scores(interactome, ENSG2idx, causal_genes, alpha):
+def calculate_scores(interactome, ENSG2idx, causal_genes, alpha, pathToCode):
     '''
     Calculate scores for every gene in the interactome based on the proximity to known causal genes.
 
@@ -66,7 +66,7 @@ def calculate_scores(interactome, ENSG2idx, causal_genes, alpha):
     returns:
     - scores: list of floats of length num_genes, value=score in the same order as causal_genes
     '''
-    so_file = "./GBA-centrality-C/gbaCentrality.so"
+    so_file = pathToCode + "/GBA-centrality-C/gbaCentrality.so"
     gbaLibrary = ctypes.CDLL(so_file)
     # declare function signature
     gbaLibrary.gbaCentrality.argtypes = [
@@ -121,7 +121,7 @@ def calculate_scores(interactome, ENSG2idx, causal_genes, alpha):
     return(scoresList)
 
 
-def main(interactome_file, causal_genes_file, uniprot_file, alpha, weighted, directed):
+def main(interactome_file, causal_genes_file, uniprot_file, alpha, weighted, directed, pathToCode):
 
     logger.info("Parsing interactome")
     (interactome, ENSG2idx) = data_parser.parse_interactome(interactome_file, weighted, directed)
@@ -133,7 +133,7 @@ def main(interactome_file, causal_genes_file, uniprot_file, alpha, weighted, dir
     causal_genes = data_parser.parse_causal_genes(causal_genes_file, gene2ENSG, ENSG2idx)
 
     logger.info("Calculating scores")
-    scores = calculate_scores(interactome, ENSG2idx, causal_genes, alpha)
+    scores = calculate_scores(interactome, ENSG2idx, causal_genes, alpha, pathToCode)
 
     logger.info("Printing scores")
     data_parser.scores_to_TSV(scores, ENSG2gene, ENSG2idx)
@@ -142,7 +142,7 @@ def main(interactome_file, causal_genes_file, uniprot_file, alpha, weighted, dir
 
 
 if __name__ == "__main__":
-    script_name = os.path.basename(sys.argv[0])
+    (pathToCode, script_name) = os.path.split(os.path.realpath(sys.argv[0]))
     # configure logging, sub-modules will inherit this config
     logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
@@ -189,12 +189,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
-        main(interactome_file=args.interactome,
-             causal_genes_file=args.causal,
-             uniprot_file=args.uniprot,
-             alpha=args.alpha,
-             weighted=args.weighted,
-             directed=args.directed)
+        main(args.interactome, args.causal, args.uniprot, args.alpha, args.weighted,
+             args.directed, pathToCode)
 
     except Exception as e:
         # details on the issue should be in the exception name, print it to stderr and die
